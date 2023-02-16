fdce69d25bc9f8501e147806d5a97079d9c99467
Ignacio Velasco <ignacio.velasco@livestories.com>
2023-02-16 16:25:55 -0300

    Init server

---
+++ src/config/vars.js
@@ -0,0 +1,17 @@
+
+const path = require('path');
+
+// import .env variables
+require('dotenv-safe').config({
+  path: path.join(__dirname, '../../.env'),
+  example: path.join(__dirname, '../../.env.example'),
+})
+
+module.exports = {
+  mongo: {
+    uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
+  },
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
+};
