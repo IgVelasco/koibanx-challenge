524c6fdcfa7f798f987f10b7084cca423dfd87bb
Ignacio Velasco <ignacio.velasco@livestories.com>
2023-02-18 19:41:47 -0300

    Fix lint issues and added logging

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -2,6 +2,7 @@ const logger = require('../config/logger')
 const ExcelToJson = require('../models/excel_to_json')
 
 exports.getStatus = async (req, res, next) => {
+  logger.info(`Fetching status for id: ${req.params.id}`)
   const status = (await ExcelToJson.get(req.params.id)).status
   res.json({ status })
 }
