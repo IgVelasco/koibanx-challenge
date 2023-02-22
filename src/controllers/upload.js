0c7f1a7707a84bd92080db583188f6f07b20ebd8
Ignacio Velasco <ignacio.velasco@livestories.com>
2023-02-22 12:32:31 -0300

    refactor of task_queue and added callback

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -3,6 +3,8 @@
 const logger = require('../config/logger')
 const TaskQueue = require('../services/task_queue')
 const ExcelToJson = require('../models/excel_to_json')
+const fileProcessing = require('../services/file_processing')
+
 const queue = new TaskQueue()
 
 exports.upload = async (req, res, next) => {
@@ -11,12 +13,14 @@ exports.upload = async (req, res, next) => {
     const excelToJson = new ExcelToJson({
       fileName: req.file.originalname,
       mapping: JSON.parse(req.body.mapping),
+      callbackUrl: req.body.callbackUrl,
     })
     await excelToJson.save() // Save the new document to the database
     const taskId = excelToJson._id
     queue.addTask({
       id: taskId,
       data: { file: req.file.buffer, mapping: JSON.parse(req.body.mapping) },
+      processTask: fileProcessing.processTask,
     })
     res.json({ taskId })
   } catch (err) {
--- src/services/task_queue.js
+++ src/services/task_queue.js
@@ -1,29 +1,28 @@
 const async = require('async')
-const xlsx = require('xlsx')
+const axios = require('axios')
 const logger = require('../config/logger')
 const { concurrencyLevels } = require('../config/vars')
-const ExcelToJson = require('../models/excel_to_json')
-const ExcelError = require('../models/excel_error')
-const utils = require('../utils/string')
 
 class TaskQueue {
-  constructor() {
+  constructor(processTask) {
     if (TaskQueue.instance) {
       return TaskQueue.instance
     }
+    this.processTask = processTask
     this.queue = async.queue(async task => {
       try {
         // Process the task
         logger.info(`Processing task ${task.id}...`)
-        await this.processTask(task)
+        const result = await task.processTask(task)
 
         // Call the callback
-        this.callback(task.id)
+        this.callback(task.id, result)
       } catch (err) {
         // Handle errors
         logger.error(err)
-        // console.error(`Error processing task ${task.id}: ${err.message}`)
+        console.error(`Error processing task ${task.id}: ${err.message}`)
         this.callback(err)
+        throw err
       }
     }, concurrencyLevels)
 
@@ -42,77 +41,9 @@ class TaskQueue {
     })
   }
 
-  callback(id) {
+  callback(id, result) {
     // Perform the callback to indicate that the task has been completedk
     logger.info(`Making callback for id ${id}`)
   }
-
-  async processTask(task) {
-    let excelToJson = await ExcelToJson.get(task.id)
-    excelToJson.status = 'processing'
-    await excelToJson.save()
-    const workbook = xlsx.read(task.data.file)
-    const sheet = workbook.Sheets[workbook.SheetNames[0]]
-    const rows = xlsx.utils.sheet_to_json(sheet, { header: 1 })
-    const headers = rows[0]
-    const data = []
-    const mapping = task.data.mapping
-
-    // await new Promise(resolve => setTimeout(resolve, 1000 * 20)); // Used for testing big excels
-
-    // Loop through the rows, starting from the second row
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i]
-      const item = {}
-
-      // Loop through the cells in the row and map to JSON
-      for (let j = 0; j < headers.length; j++) {
-        const value = row[j]
-
-        // Get the mapping for the current column
-        const mappingInfo = mapping[xlsx.utils.encode_col(j)]
-
-        if (mappingInfo) {
-          // TODO: add object support (not asked but NTH)
-          const { name, type } = mappingInfo
-          // Perform type conversion if necessary
-          try {
-            if (type === 'number') {
-              if (utils.containsOnlyNumbers(value)) {
-                item[name] = parseFloat(value)
-              } else {
-                throw Error(`Value: ${value} is not full number`)
-              }
-            } else {
-              item[name] = value
-            }
-          } catch (error) {
-            await new ExcelError({
-              row: i + 1,
-              column: xlsx.utils.encode_col(j),
-              error: error.message,
-              excelId: task.id,
-            }).save()
-            excelToJson = await excelToJson.incrementErrorCount()
-            item[name] = null
-          }
-        }
-      }
-      data.push(item)
-    }
-
-    excelToJson.status = 'done'
-    excelToJson.output = data
-    await excelToJson.save()
-  }
 }
 module.exports = TaskQueue
-
-// // Example usage:
-// const taskQueue = new TaskQueue(2); // Set concurrency level to 2
-
-// const task1 = { id: 1, data: 'Task 1 data' };
-// const task2 = { id: 2, data: 'Task 2 data' };
-
-// taskQueue.addTask(task1);
-// taskQueue.addTask(task2);
